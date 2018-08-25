b6073b4c34cb7bc9bd520b4465d32e6d21b49129
bylee <vonin85@gmail.com>
2018-08-25 18:36:32 +0900

    배포 스크립트 경로 변경

--- TagWeb/script/deploy.sh
+++ TagWeb/script/deploy.sh
@@ -18,11 +18,11 @@ BASE=$(cd $(dirname $(readlink $0 || echo $0))/..;/bin/pwd)
 HOST=midnightclub.kr
 
 ## copy
-echo "scp $BASE/WebContent/*.jsp uadmin@$HOST:/uadmin/html/main/"
-scp $BASE/WebContent/*.jsp uadmin@$HOST:/uadmin/html/main/
+echo "scp $BASE/WebContent/*.jsp uadmin@$HOST:/uadmin/tomcat"
+scp $BASE/WebContent/*.jsp uadmin@$HOST:/uadmin/tomcat
 
 if [[ $ARGV1 == "all" ]]; then
-    scp -r $BASE/WebContent/assets uadmin@$HOST:/uadmin/html/main/
-    scp -r $BASE/WebContent/pages uadmin@$HOST:/uadmin/html/main/
+    scp -r $BASE/WebContent/assets uadmin@$HOST:/uadmin/tomcat
+    scp -r $BASE/WebContent/pages uadmin@$HOST:/uadmin/tomcat
 fi
 
